cae4c65e2f90ef8d465075ae69a0917f009b4c7b
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-05-27 01:13:45 +0600

    added explanation of arbitrary *args and **kwargs and super class references in the start point scripts init method in Redis Cache

--- redis-cache/index.py
+++ redis-cache/index.py
@@ -10,6 +10,13 @@ import mysql_gateway
 import config
 
 class HttpHandler(http.server.SimpleHTTPRequestHandler):
+    """_summary_
+        This line ensures that the HttpHandler class is properly initialized as a SimpleHTTPRequestHandler. 
+        The SimpleHTTPRequestHandler expects certain parameters (request, client_address, and server), 
+        which are passed by socketserver.TCPServer when handling a request. 
+        By including arbitrary positional & keywords arguments, *args and **kwargs, 
+        we ensure these parameters are correctly forwarded to the superclass initializer.
+    """
     def __init__(self, *args, **kwargs):
         self.redis_server = redis.Redis(
             host=config.redis_host,
@@ -17,7 +24,11 @@ class HttpHandler(http.server.SimpleHTTPRequestHandler):
             db=config.redis_db_number,
             password=config.redis_password
         )
-        
+        """
+            super(): This function returns a temporary object of the superclass (http.server.SimpleHTTPRequestHandler)
+            that allows you to call its methods.
+            super().__init__(*args, **kwargs): Calls the initializer of the superclass with the arguments received.
+        """
         super().__init__(*args, **kwargs)
     
     def do_GET(self):
@@ -55,7 +66,11 @@ class HttpHandler(http.server.SimpleHTTPRequestHandler):
             }
             
         self.wfile.write(bytes(json.dumps(resp, indent = 2), "utf8"))
-            
+
+"""
+socketserver.TCPServer will pass additional arguments (request, client_address, and server) 
+when it creates an instance of HttpHandler.
+"""
 httpd = socketserver.TCPServer(("", 8080), HttpHandler)
 print("The HTTP server is running at port 8080...")
 
